8b3522fcca6bc930f78d4f46a43e18cfdb263f9c
123456-manvi <103314650+123456-manvi@users.noreply.github.com>
2024-08-07 02:23:55 +0530

    Update Intro.cpp

--- Intro.cpp
+++ Intro.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 class Hero {
-private:
+public:
 int health;
 
 public:
@@ -32,22 +32,42 @@ void setLevel(char ch){
 };
 
 int main(){
-  // creation of object
-  Hero ramesh;
   
-//   to use private fun use getter functiion like that:
-  cout<<"Ramesh health is: "<<ramesh.getHealth()<<endl;
-  //ramesh.health=70;
+// static allocation
+  Hero a;
+  a.setHealth(80);
+  a.setLevel('A');
+  cout<<"level is"<<a.level<<endl;
+  cout<<"health is"<<a.getHealth()<<endl;
   
-  
-//   to use setter fun use setter function
-    ramesh.setHealth(70);
-  ramesh.level='A';
-  
-// cout<<"ramesh health"<<ramesh.health<<endl;
+  // dynamically...........
+  Hero *b=new Hero;
+  b->setLevel('B');
+  b->setHealth(70);
+  cout<<"level is"<<(*b).level<<endl;
+  cout<<"health is"<<(*b).getHealth()<<endl;
 
-cout<<"ramesh health: "<<ramesh.getHealth()<<endl;
-cout<<"ramesh level: "<<ramesh.level<<endl;
+
+  cout<<"level is"<<b->level<<endl;
+  cout<<"health is"<<b->getHealth()<<endl;
+
+  // ..................
+//   // creation of object
+//   Hero ramesh;
+  
+// //   to use private fun use getter functiion like that:
+//   cout<<"Ramesh health is: "<<ramesh.getHealth()<<endl;
+//   //ramesh.health=70;
+  
+  
+// //   to use setter fun use setter function
+//     ramesh.setHealth(70);
+//   ramesh.level='A';
+  
+// // cout<<"ramesh health"<<ramesh.health<<endl;
+
+// cout<<"ramesh health: "<<ramesh.getHealth()<<endl;
+// cout<<"ramesh level: "<<ramesh.level<<endl;
 
 return 0;
 }
